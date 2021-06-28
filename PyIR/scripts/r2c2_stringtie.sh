25c13d7566b72084e6db071172966daf0a838300
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2021-06-28 23:27:25 +0200

    added PyIR

---
+++ PyIR/scripts/r2c2_stringtie.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+## Pass positional variable(s)
+NTHREADS=$1
+NTHREADS="${NTHREADS:=12}"
+echo "Processing with $NTHREADS thread(s)." 
+
+echo "Running minimap2." 
+
+## Create folders
+mkdir /mnt/0/output_minimap2
+
+
+## Run the postprocessing script
+minimap2 -G 400k --secondary=no -ax splice:hq -t $NTHREADS /mnt/1/hg38.fa /mnt/0/output_c3poa_postprocessing/R2C2_full_length_consensus_reads.fasta > /mnt/0/output_minimap2/out.SAM  
+
+
+echo "Running samtools." 
+
+
+## Create folders
+mkdir /mnt/0/output_samtools
+
+
+## Run the script
+# samtools view -Su /mnt/0/output_minimap2/out.SAM | samtools sort /mnt/0/output_samtools/alns.sorted.BAM
+samtools view -Su /mnt/0/output_minimap2/out.SAM | samtools sort -o /mnt/0/output_samtools/alns.sorted.BAM
+
+echo "Running stringtie." 
+mkdir /mnt/0/output_stringtie
+
+stringtie /mnt/0/output_samtools/alns.sorted.BAM -o /mnt/0/output_stringtie/out.gtf
+
+echo "done!"							
+# Exit the script
+exit
