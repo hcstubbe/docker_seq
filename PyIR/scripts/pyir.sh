0badd50abb5724c4e3cb6448ec3ce8e3506f5a86
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2021-06-29 16:29:00 +0200

    Update pyir.sh

--- PyIR/scripts/pyir.sh
+++ PyIR/scripts/pyir.sh
@@ -6,31 +6,14 @@ NTHREADS=$1
 NTHREADS="${NTHREADS:=12}"
 echo "Processing with $NTHREADS thread(s)." 
 
-echo "Running minimap2." 
+echo "Running PyIR." 
 
 ## Create folders
-mkdir /mnt/0/output_minimap2
+mkdir /mnt/0/output_pyir
+mkdir /mnt/0/tmp_pyir
 
+pyir /mnt/0/output_c3poa/UMI_Splint_1/R2C2_Consensus.fasta -m $NTHREADS --tmp_dir /mnt/0/tmp_pyir 
 
-## Run the postprocessing script
-minimap2 -G 400k --secondary=no -ax splice:hq -t $NTHREADS /mnt/1/hg38.fa /mnt/0/output_c3poa_postprocessing/R2C2_full_length_consensus_reads.fasta > /mnt/0/output_minimap2/out.SAM  
-
-
-echo "Running samtools." 
-
-
-## Create folders
-mkdir /mnt/0/output_samtools
-
-
-## Run the script
-# samtools view -Su /mnt/0/output_minimap2/out.SAM | samtools sort /mnt/0/output_samtools/alns.sorted.BAM
-samtools view -Su /mnt/0/output_minimap2/out.SAM | samtools sort -o /mnt/0/output_samtools/alns.sorted.BAM
-
-echo "Running stringtie." 
-mkdir /mnt/0/output_stringtie
-
-stringtie /mnt/0/output_samtools/alns.sorted.BAM -o /mnt/0/output_stringtie/out.gtf
 
 echo "done!"							
 # Exit the script
