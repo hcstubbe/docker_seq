bcade76e594df3bf4613cb8496d3caa3f8333f3c
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2021-07-05 23:43:32 +0200

    Update c3poa_main_slurm_cm2tiny.cmd

--- c3poa/scripts/c3poa_main_slurm_cm2tiny.cmd
+++ c3poa/scripts/c3poa_main_slurm_cm2tiny.cmd
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mail-type=end
-#SBATCH --mail-user=hstubbe@med.lmu.de
+#SBATCH --mail-user=[MAIL ADDRESS]
 #SBATCH --export=NONE
 #SBATCH --time=12:00:00
 
