7b3de6f6a3a10ce51d0e24b15a37d156e140af13
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2021-07-06 18:24:04 +0200

    Update run_pipeline.cmd

--- run_pipeline.cmd
+++ run_pipeline.cmd
@@ -8,14 +8,14 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mail-type=end
-#SBATCH --mail-user=hstubbe@med.lmu.de
+#SBATCH --mail-user=[MAIL ADDRESS]
 #SBATCH --export=NONE
 #SBATCH --time=12:00:00
 
 module load charliecloud
 
-ch-run -w ~/ccl_images/c3poa -b ~/seq_data/CORKUM_05/ -- sh /app/c3poa.sh 24 900 450 1000
+ch-run -w ~/ccl_images/c3poa -b ~/path/to/sample/ -- sh /app/c3poa.sh 24 900 450 1000
 
-ch-run -w ~/ccl_images/pyir -b ~/seq_data/CORKUM_05/  -b ~/seq_data/ref_data/ -- sh /app/scripts/pyir.sh 24
+ch-run -w ~/ccl_images/pyir -b ~/path/to/sample/  -b ~/path/to/ref_data/ -- sh /app/scripts/pyir.sh 24
 
-ch-run -w ~/ccl_images/r2c2_stringtie -b ~/seq_data/CORKUM_05/  -b ~/seq_data/ref_data/ -- sh /app/r2c2_stringtie.sh
+ch-run -w ~/ccl_images/r2c2_stringtie -b ~/path/to/sample/  -b ~/path/to/ref_data/ -- sh /app/r2c2_stringtie.sh
