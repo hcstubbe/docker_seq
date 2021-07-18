60218a9570d07b36e3e007f4793b187eac122e88
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2021-07-18 14:03:30 +0200

    Update run_pipeline.cmd

--- run_pipeline.cmd
+++ run_pipeline.cmd
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J pipe
+#SBATCH -J spipe
 #SBATCH -o ./seq_data/logfiles/%x.%j.%N.out
 #SBATCH -e ./seq_data/logfiles/%x.%j.%N.error
 #SBATCH --get-user-env
@@ -8,9 +8,10 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mail-type=end
-#SBATCH --mail-user=[MAIL ADDRESS]
+#SBATCH --mail-user=hstubbe@med.lmu.de
 #SBATCH --export=NONE
 #SBATCH --time=12:00:00
+
 
 module load charliecloud
 
@@ -18,4 +19,4 @@ ch-run -w ~/ccl_images/c3poa -b ~/path/to/sample/ -- sh /app/c3poa.sh 24 900 450
 
 ch-run -w ~/ccl_images/pyir -b ~/path/to/sample/  -b ~/path/to/ref_data/ -- sh /app/scripts/pyir.sh 24
 
-ch-run -w ~/ccl_images/r2c2_stringtie -b ~/path/to/sample/  -b ~/path/to/ref_data/ -- sh /app/r2c2_stringtie.sh
+ch-run -w ~/ccl_images/r2c2_stringtie -b ~/path/to/sample/  -b ~/path/to/ref_data/ -- sh /app/r2c2_stringtie.sh 24
