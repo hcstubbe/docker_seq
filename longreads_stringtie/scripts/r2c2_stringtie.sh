dc0fa182e993f24b7f10959b51a2e3ce19f8c5ac
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2021-07-27 00:18:35 +0200

    Update r2c2_stringtie.sh

--- longreads_stringtie/scripts/r2c2_stringtie.sh
+++ longreads_stringtie/scripts/r2c2_stringtie.sh
@@ -13,7 +13,7 @@ mkdir /mnt/0/output_minimap2
 
 
 ## Run the postprocessing script
-minimap2 -G 400k --secondary=no -ax splice:hq -t $NTHREADS /mnt/1/hg38.fa /mnt/0/output_c3poa_postprocessing/R2C2_full_length_consensus_reads.fasta > /mnt/0/output_minimap2/out.SAM  
+minimap2 -G 400k -ax splice:hq -uf -t $NTHREADS /mnt/1/hg38.fa /mnt/0/output_c3poa_postprocessing/R2C2_full_length_consensus_reads.fasta > /mnt/0/output_minimap2/out.SAM  
 
 
 echo "Running samtools." 
