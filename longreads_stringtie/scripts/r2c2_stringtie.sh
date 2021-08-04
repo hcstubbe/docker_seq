823f4877a9e95ed23c2400b9eba2fd3362649cc2
Hans Stubbe <ru75jul2@cm2login2.cos.lrz.de>
2021-08-04 13:43:58 +0200

    Updated stringtie for ballgown anaysis.  Please enter the commit message for your changes. Lines starting

--- longreads_stringtie/scripts/r2c2_stringtie.sh
+++ longreads_stringtie/scripts/r2c2_stringtie.sh
@@ -30,14 +30,12 @@ samtools view -Su /mnt/0/output_minimap2/out.SAM | samtools sort -o /mnt/0/outpu
 echo "Running stringtie." 
 mkdir /mnt/0/output_stringtie
 
-stringtie /mnt/0/output_samtools/alns.sorted.BAM \
-                          -v \
-                          -p $NTHREADS \
-                          -G /mnt/1/gencode.v29.annotation.gtf \
-                          -A /mnt/0/output_stringtie/gene_abund.tab \
-                          -C /mnt/0/output_stringtie/cov_refs.gtf \
-                          -o /mnt/0/output_stringtie/out.gtf \
-						  -L
+stringtie   -p $NTHREADS \
+            -G /mnt/1/gencode.v29.annotation.gtf \
+            -o /mnt/0/output_stringtie/out.gtf \
+            -l STRG \
+            -L \
+            /mnt/0/output_samtools/alns.sorted.BAM \
 
 echo "done!"							
 # Exit the script
--- longreads_stringtie/scripts/stringtie.sh
+++ longreads_stringtie/scripts/stringtie.sh
@@ -10,15 +10,12 @@ echo "Processing with $NTHREADS thread(s)."
 echo "Running stringtie." 
 mkdir /mnt/0/output_stringtie
 
-stringtie /mnt/0/output_samtools/alns.sorted.BAM \
-                          -v \
-                          -p $NTHREADS \
-                          -G /mnt/1/gencode.v29.annotation.gtf \
-                          -A /mnt/0/output_stringtie/gene_abund.tab \
-                          -C /mnt/0/output_stringtie/cov_refs.gtf \
-                          -o /mnt/0/output_stringtie/out.gtf \
-						  -L \
-						  -B
+stringtie   -p $NTHREADS \
+            -G /mnt/1/gencode.v29.annotation.gtf \
+            -o /mnt/0/output_stringtie/out.gtf \
+            -l STRG \
+            -L \
+            /mnt/0/output_samtools/alns.sorted.BAM \
 
 echo "done!"							
 # Exit the script
