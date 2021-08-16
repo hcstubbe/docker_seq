ec87797aaadec9b4e4686d0626ccf8f75cbc9d03
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2021-08-17 01:21:35 +0200

    Update pomoxis.sh

--- pomoxis/scripts/pomoxis.sh
+++ pomoxis/scripts/pomoxis.sh
@@ -5,11 +5,25 @@ NTHREADS=$1
 NTHREADS="${NTHREADS:=12}"
 echo "Processing with $NTHREADS thread(s)." 
 
-## Run pomoxis program mini_assemble
-echo "Running pmoxis tools: mini_assemble" 
-mkdir /mnt/0/output_pomoxis_mapping
+## Run pomoxis programs
 
+# De novo assembly
+echo "Running pmoxis tools: mini_assemble" 
+mkdir /mnt/0/output_mini_assemble
 mini_assemble  -i /mnt/0/output_bonito/all.fastq
-               # -r /mnt/1/hg38.fa \
                -o /mnt/0/output_mini_assemble/ \
                -t $NTHREADS
+               
+               
+              
+# Alining reads
+echo "Running pmoxis tools: mini_align" 
+mkdir /mnt/0/output_mini_align
+mini_align  -i /mnt/0/output_bonito/all.fastq
+            -o /mnt/0/output_mini_align/ \
+            -t $NTHREADS \
+            -r /mnt/1/hg38.fa \
+            -n
+               
+               
+echo "Done!               
