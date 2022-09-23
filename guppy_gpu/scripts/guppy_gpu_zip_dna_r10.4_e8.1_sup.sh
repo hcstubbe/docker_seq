08a48cc1036756ab4d3847b2bba7ccd91e66cd68
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2022-09-23 19:08:06 +0200

    Update guppy_gpu_zip_dna_r10.4_e8.1_sup.sh

--- guppy_gpu/scripts/guppy_gpu_zip_dna_r10.4_e8.1_sup.sh
+++ guppy_gpu/scripts/guppy_gpu_zip_dna_r10.4_e8.1_sup.sh
@@ -3,16 +3,16 @@
 
 ## Unzip fast5 files to /data/tmp/
 mkdir -p /data/tmp/
-mkdir -p /data/fast5/
+mkdir -p /data/tmp_fast5/
 unzip /data/*.zip -d /data/tmp/
-find /data/tmp/ -iname *.fast5 -exec mv {} /data/fast5/ \;
+find /data/tmp/ -iname *.fast5 -exec mv {} /data/tmp_fast5/ \;
 rm -r /data/tmp/
 mkdir -p /data/basecalled/
 
 
 ## Run the basecaller
-guppy_basecaller -i /data/fast5/ -s /data/basecalled/ -c dna_r10.4_e8.1_sup.cfg -x "cuda:0"
+guppy_basecaller -i /data/tmp_fast5/ -s /data/basecalled/ -c dna_r10.4_e8.1_sup.cfg -x "cuda:0"
 
 
 ## Remove the unzipped fast5 files
-rm -r /data/fast5/
+rm -r /data/tmp_fast5/
