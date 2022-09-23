d90cf501b3c6a861bd1bba7ed103ac272af506a0
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2022-09-23 20:18:33 +0200

    Create guppy_gpu_fast5_dna_r10.4_e8.1_sup.sh

---
+++ guppy_gpu/scripts/guppy_gpu_fast5_dna_r10.4_e8.1_sup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+## Unzip fast5 files to /data/tmp/
+mkdir -p /data/tmp_fast5/
+find /data/ -iname *.fast5 -exec mv {} /data/tmp_fast5/ \;
+mkdir -p /data/basecalled/
+
+
+## Run the basecaller
+guppy_basecaller -i /data/tmp_fast5/ -s /data/basecalled/ -c dna_r10.4_e8.1_sup.cfg -x "cuda:0"
+
+
+## Remove the unzipped fast5 files
+rm -r /data/tmp_fast5/
