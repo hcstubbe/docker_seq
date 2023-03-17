34c32792dbc406e1d8b49d9280b252d232bdfc79
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2023-03-18 00:33:24 +0100

    Update

--- guppy_gpu/scripts/test_run_dna_r10.4_e8.1_sup - Kopie.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATAPATH=[/path/to/data/]
-GUPPYCFG=dna_r10.4_e8.1_sup.cfg ## e.g. dna_r10.4_e8.1_sup.cfg or dna_r10.3_450bps_sup.cfg
-INPUTTYPE=zip ## zip, tar.gz or fast5
- 
-srun --mpi=pmi2 --container-mounts=$DATAPATH:/data/ \
-     --container-image='hstubbe/guppy_gpu' \
-     sh /app/guppy_gpu_$INPUTTYPE.sh $GUPPYCFG
