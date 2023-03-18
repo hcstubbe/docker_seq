b90c34f9fdd0092f12123d09d39921317f29d3db
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2023-03-18 02:47:16 +0100

    üudate

--- guppy_gpu/scripts/guppy_gpu_fast5.sh
+++ guppy_gpu/scripts/guppy_gpu_fast5.sh
@@ -2,6 +2,7 @@
 
 GUPPYSCRIPT=$1
 
+find . -type f -name "*dna_r10.4.1_e8.2_260bps*" > /mnt/data/models.txt
 
 ## Move fast5 files to /mnt/data/tmp/
 mkdir -p /mnt/data/tmp_fast5/
@@ -13,7 +14,7 @@ mkdir -p /mnt/data/basecalled/
 
 
 ## Run the basecaller
-guppy_basecaller --do_read_splitting -i /mnt/data/tmp_fast5/ -s /mnt/data/basecalled/ -c $GUPPYSCRIPT -x "cuda:0"
+guppy_basecaller -i /mnt/data/tmp_fast5/ -s /mnt/data/basecalled/ -c $GUPPYSCRIPT -x "cuda:0" --do_read_splitting
 
 
 ## Do NOT remove the fast5 files
