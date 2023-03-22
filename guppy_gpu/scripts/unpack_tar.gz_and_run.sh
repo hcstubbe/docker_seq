58036ea8b83259fd854e74c59f88a9450acd6105
hcstubbe <48794922+hcstubbe@users.noreply.github.com>
2023-03-23 00:46:59 +0100

    Update unpack_tar.gz_and_run.sh

--- guppy_gpu/scripts/unpack_tar.gz_and_run.sh
+++ guppy_gpu/scripts/unpack_tar.gz_and_run.sh
@@ -4,15 +4,15 @@ DATAPATH=/dss/dssfs02/lwp-dss-0001/u7x81/u7x81-dss-0000/ru75jul2/seq_data/ir_seq
 SBATCHFILE=./run_dna_r10.4.1_e8.2_sup.sbatch
 
 ## Untar and decompress fast5 files to $DATAPATH/tmp/
-print "Creating directories for fast5 files and reports..."
+echo "Creating directories for fast5 files and reports..."
 mkdir -p $DATAPATH/tmp/
 mkdir -p $DATAPATH/tmp_fast5/
 mkdir -p $DATAPATH/reports/
 
-print "Start decompressing fast5 files..."
+echo "Start decompressing fast5 files..."
 tar -xzvf $DATAPATH/*.tar.gz -C $DATAPATH/tmp/
 
-print "Sort files into appropriate folders..."
+echo "Sort files into appropriate folders..."
 find $DATAPATH/ -iname *.csv -exec mv {} $DATAPATH/reports/ \;
 find $DATAPATH/ -iname *.pdf -exec mv {} $DATAPATH/reports/ \;
 find $DATAPATH/ -iname *.html -exec mv {} $DATAPATH/reports/ \;
@@ -20,4 +20,5 @@ find $DATAPATH/ -iname *.fast5 -exec mv {} $DATAPATH/tmp_fast5/ \;
 
 
 # Run sbtach file (make sure the file is configured correctly)
+echo "Running sbtach"
 sbatch $SBATCHFILE
